0e72444968c398577fbc15530345c9112db53fef
Garudo24 <ff2141soccer@yahoo.co.jp>
2020-12-06 11:25:39 +0900

    マイグレーションファイル仮実装

---
+++ database/migrations/2020_12_05_065406_create_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->string('id', 255)->primary()->nullable(false);
+            $table->integer('user_id')->unique()->nullable(false)->foreign()->regerences('id')->on('users');
+            $table->string('filename', 255)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
