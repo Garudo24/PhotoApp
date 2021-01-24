c568d01203f089c7649ada2dcb462162a70590c6
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-01-24 15:17:47 +0900

    バリデーションエラーの場合位の分岐を作成

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,8 +1,10 @@
 import { OK } from '../util'
+import { OK, UNPROCESSABLE_ENTITY } from '../util'
 
 const state = {
     user: null,
     apiStatus: null,
+    loginErrorMessages: null
 }
 
 const getters = {
@@ -16,6 +18,9 @@ const mutations = {
     },
     setApiStatus(state, status) {
         state.apiStatus = status
+    },
+    setLoginErrorMessages(state, messages) {
+        state.loginErrorMessages = messages
     }
 }
 
@@ -36,7 +41,11 @@ const actions = {
         }
 
         context.commit('setApiStatus', false)
-        context.commit('error/setCode', response.status, { root: true })
+        if (response.status === UNPROCESSABLE_ENTITY) {
+            context.commit('setLoginErrorMessages', response.data.errors)
+        } else {
+            context.commit('error/setCode', response.status, { root: true })
+        }
     },
     async logout(context) {
         const response = await axios.post('/logout')
