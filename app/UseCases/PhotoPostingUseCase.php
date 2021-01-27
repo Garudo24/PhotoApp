c7c93e32720670be86f04acf8590c1b1014ea404
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-01-27 23:35:09 +0900

    認証済みuserのidを受け取って、Photoモデルに書き込むよう実装

--- app/UseCases/PhotoPostingUseCase.php
+++ app/UseCases/PhotoPostingUseCase.php
@@ -2,12 +2,22 @@
 
 namespace App\UseCases;
 
+use App\Models\Photo;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class PhotoPostingUseCase
 {
-    public function execute($image_file)
+    public function execute($user_id, $image_file)
     {
         Storage::cloud()->putFileAs('', $image_file, $image_file->name);
+
+        DB::transaction(function () use ($user_id, $image_file) {
+            Photo::create([
+                'user_id' => $user_id,
+                'filename' => $image_file->name
+            ]);
+        });
     }
 }
