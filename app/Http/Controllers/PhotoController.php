3436e10de2bb2573c91aa565d5dacc14d1542ac6
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-05-12 01:13:43 +0900

    ダウンロード用のController実装、写真投稿用のコントローラーでUseCaseにロジックを寄せたため、ControllerからUseCaseを呼び出す処理を追加(仮実装)

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePhoto;
 use App\Models\Photo;
+use App\UseCases\PhotoPostingUseCase;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PhotoController extends Controller
 {
@@ -21,10 +26,35 @@ class PhotoController extends Controller
     /**
      * 写真投稿
      * @param StorePhoto $request
+     * @param PhotoPostingUseCase $usecase
      * @return \Illuminate\Http\Request
      */
-    public function create(StorePhoto $request)
+    public function create(StorePhoto $request, PhotoPostingUseCase $usecase)
     {
+        $photo = new Photo();
+        $file = new File($request->photo);
+        $usecase->execute(Auth::id(), $file);
         return response('', 201);
+    }
+
+    /**
+ * 写真ダウンロード
+ * @param Photo $photo
+ * @return \Illuminate\Http\Response
+ */
+    public function download(Photo $photo)
+    {
+        // 写真の存在チェック
+        if (! Storage::cloud()->exists($photo->filename)) {
+            abort(404);
+        }
+
+        $disposition = 'attachment; filename="' . $photo->filename . '"';
+        $headers = [
+        'Content-Type' => 'application/octet-stream',
+        'Content-Disposition' => $disposition,
+    ];
+
+        return response(Storage::cloud()->get($photo->filename), 200, $headers);
     }
 }
