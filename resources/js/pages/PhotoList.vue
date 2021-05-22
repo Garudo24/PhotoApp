d39e6e5e6ce11a2592f8564fd3ecec520007ee8a
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-05-22 23:05:28 +0900

    写真の一覧をひょうじするためのコンポーネントを作成

--- resources/js/pages/PhotoList.vue
+++ resources/js/pages/PhotoList.vue
@@ -8,31 +8,43 @@
         :item="photo"
       />
     </div>
+    <Pagination :current-page="currentPage" :last-page="lastPage" />
   </div>
 </template>
 
 <script>
+import { OK } from "../util";
 import Photo from "../components/Photo.vue";
-
+import Pagination from "../components/Pagination.vue";
 export default {
   components: {
     Photo,
+    Pagination,
+  },
+  props: {
+    page: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
   },
   data() {
     return {
       photos: [],
+      currentPage: 0,
+      lastPage: 0,
     };
   },
   methods: {
     async fetchPhotos() {
-      const response = await axios.get("/photos");
-
+      const response = await axios.get(`/photos/?page=${this.page}`);
       if (response.status !== OK) {
         this.$store.commit("error/setCode", response.status);
         return false;
       }
-
       this.photos = response.data.data;
+      this.currentPage = response.data.current_page;
+      this.lastPage = response.data.last_page;
     },
   },
   watch: {
