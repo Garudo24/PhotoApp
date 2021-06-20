480dba22fed404150d0c896ac8c5460307256913
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-06-21 00:05:07 +0900

    写真にコメントを追加するUseCaseのテスト仮実装

---
+++ tests/UseCases/PhotoAddCommentUseCaseTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\UseCases;
+
+use App\Models\Comment;
+use App\Models\Photo;
+use App\Models\User;
+use App\UseCases\PhotoAddCommentUseCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class PhotoAddCommentUseCaseTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $photo;
+    private $user;
+    private $usecase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->photo = factory(Photo::class)->create([
+            'user_id' => $this->user->id
+        ]);
+        $this->usecase = new PhotoAddCommentUseCase();
+    }
+
+    /** @test */
+    public function コメントを追加できる()
+    {
+        $this->usecase->execute($this->user->id, $this->photo->id, $comment = 'テストコメント');
+        $this->assertCount(1, Comment::get());
+    }
+}
