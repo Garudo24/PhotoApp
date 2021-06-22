0a3be322133fffb14c2d838425c357861657bf2f
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-06-22 22:54:58 +0900

    commentに対して、photo_idも保存する

--- app/UseCases/PhotoAddCommentUseCase.php
+++ app/UseCases/PhotoAddCommentUseCase.php
@@ -7,13 +7,14 @@ use App\Models\Photo;
 
 class PhotoAddCommentUseCase
 {
-    public function execute(int $user_id, int $photo_id, String $comment_text)
+    public function execute(int $user_id, String $photo_id, String $comment_text)
     {
         $comment = new Comment();
         $comment->user_id = $user_id;
+        $comment->photo_id = $photo_id;
         $comment->content = $comment_text;
         $comment->save();
-        $photo = Photo::firstWhere('id', $photo_id);
+        $photo = Photo::where('id', $photo_id)->first();
         $photo->comments()->save($comment);
 
         return Comment::where('id', $comment->id)->with('user')->first();
