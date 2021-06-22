aafa7b51d5568db43ac633029895cd8467fafda5
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-06-22 23:06:47 +0900

    トランザクションを貼る

--- app/UseCases/PhotoAddCommentUseCase.php
+++ app/UseCases/PhotoAddCommentUseCase.php
@@ -4,18 +4,29 @@ namespace App\UseCases;
 
 use App\Models\Comment;
 use App\Models\Photo;
+use Illuminate\Support\Facades\DB;
 
 class PhotoAddCommentUseCase
 {
     public function execute(int $user_id, String $photo_id, String $comment_text)
     {
+        $arguments = [
+            'user_id' => $user_id,
+            'photo_id' => $photo_id,
+            'comment_text' => $comment_text
+        ];
         $comment = new Comment();
-        $comment->user_id = $user_id;
-        $comment->photo_id = $photo_id;
-        $comment->content = $comment_text;
-        $comment->save();
-        $photo = Photo::where('id', $photo_id)->first();
-        $photo->comments()->save($comment);
+
+        DB::transaction(function () use ($comment, $arguments) {
+            $comment = new Comment();
+            $comment->user_id = $arguments['user_id'];
+            $comment->photo_id = $arguments['photo_id'];
+            $comment->content = $arguments['comment_text'];
+            $comment->save();
+
+            $photo = Photo::where('id', $arguments['photo_id'])->first();
+            $photo->comments()->save($comment);
+        });
 
         return Comment::where('id', $comment->id)->with('user')->first();
     }
