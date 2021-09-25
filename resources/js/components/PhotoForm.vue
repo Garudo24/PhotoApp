5de83a571b30df9858ab76ed92e83dd4747dc58f
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-09-25 20:25:28 +0900

    jsonで返却されたIDをもとに、アップロードした画像の詳細画面に遷移する

--- resources/js/components/PhotoForm.vue
+++ resources/js/components/PhotoForm.vue
@@ -105,7 +105,7 @@ export default {
         timeout: 6000,
       })
 
-      this.$router.push(`/photos/${response.data.id}`)
+      this.$router.push(`/photos/${response.data.add_photo_id}/`)
     },
   },
 }
