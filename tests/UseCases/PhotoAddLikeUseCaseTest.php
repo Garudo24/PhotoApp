17692b28d50c915706648420b818664320637ad8
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-09-26 23:53:56 +0900

    同じユーザーが同じ写真にいいねした際の例外処理のテスト作成

--- tests/UseCases/PhotoAddLikeUseCaseTest.php
+++ tests/UseCases/PhotoAddLikeUseCaseTest.php
@@ -29,7 +29,38 @@ class PhotoAddLikeUseCaseTest extends TestCase
         $this->actingAs($this->user);
         $this->usecase->execute((String)$photo->id, $this->user->id);
 
-        $photo = Photo::with('likes')->get();
-        $this->assertCount(1, $photo->likes);
+        $photo_with_like = Photo::with('likes')->first();
+        $this->assertCount(1, $photo_with_like->likes);
+    }
+
+    /** @test */
+    public function いいねを追加した写真のIDが返ってくる()
+    {
+        $photo = factory(Photo::class)->create();
+        factory(Photo::class, 2)->create();
+        $this->actingAs($this->user);
+        $photo_id = $this->usecase->execute((String)$photo->id, $this->user->id);
+
+        $this->assertCount(3, Photo::get());
+        $this->assertEquals($photo->id, $photo_id);
+    }
+
+    /** @test */
+    public function 写真が存在しない場合nullが返る()
+    {
+        $this->actingAs($this->user);
+        $this->assertNull($this->usecase->execute('1', $this->user->id));
+    }
+
+    /** @test */
+    public function すでにいいねを行っている場合nullが返る()
+    {
+        $photo = factory(Photo::class)->create();
+        factory(Photo::class, 2)->create();
+        $this->actingAs($this->user);
+        $this->usecase->execute((String)$photo->id, $this->user->id);
+        $photo_id = $this->usecase->execute((String)$photo->id, $this->user->id);
+
+        $this->assertNull($photo_id);
     }
 }
