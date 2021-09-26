63e59ce1343c2bef0bceb8edf93c0e4bdf98ce9a
Garudo24 <ff2141soccer@yahoo.co.jp>
2021-09-27 00:04:25 +0900

    いいね機能に関するルーティングで、パラメーター名を修正

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::group(['middleware' => 'auth'], function () {
 });
 
 Route::get('/photos', 'PhotoController@index')->name('photo.index');
-Route::put('/photos/{id}/like', 'PhotoController@like')->name('photo.like');
-Route::delete('/photos/{id}/like', 'PhotoController@unlike');
+Route::put('/photos/{photo_id}/like', 'PhotoController@like')->name('photo.like');
+Route::delete('/photos/{photo_id}/like', 'PhotoController@unlike')->name('photo.unlike');
 
 Route::get('/{any?}', fn () => view('index'))->where('any', '.+');
